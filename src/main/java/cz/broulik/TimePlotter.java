2f60b525198be97818e36cd8180e18080ccce7c3
Hurda <thehurda@gmail.com>
2017-01-05 00:01:31 +0100

    added bottom-70-Quick dataset
    add highlight of safe temp and length calculatin

--- src/main/java/cz/broulik/TimePlotter.java
+++ src/main/java/cz/broulik/TimePlotter.java
@@ -4,8 +4,11 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.plot.IntervalMarker;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.ValueMarker;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.XYDataItem;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -31,6 +34,7 @@ public class TimePlotter extends JFrame {
 
     private static final Logger log = LoggerFactory.getLogger(TimePlotter.class);
     public static final int SAFE_TEMP = 63;
+    private final XYPlot plot;
 
     public static void main(String... args) throws Exception {
         new TimePlotter();
@@ -40,26 +44,20 @@ public class TimePlotter extends JFrame {
     protected XYSeriesCollection datasetXY;
 
 
+
     public TimePlotter() throws Exception {
         super("Time Plotter");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(new Dimension(300, 300));
+        setSize(new Dimension(900, 600));
+        setLocation(100, 100);
         setVisible(true);
 
         datasetXY = new XYSeriesCollection();
 
-        File dri = new File(".");
-        for (File file : dri.listFiles()) {
-            if (file.getName().endsWith(".csv")) {
-                try {
-                    datasetXY.addSeries(loadSeries(file));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
+        loadFiles();
         matchMax();
+
+
 
         chartXY = ChartFactory.createXYLineChart(
                 "Plot", // chart title
@@ -70,50 +68,80 @@ public class TimePlotter extends JFrame {
                 true, // include legend
                 true, // tooltips
                 false // urls
+
         );
 
-        chartXY.getXYPlot().addRangeMarker(new ValueMarker(SAFE_TEMP));
+        plot = chartXY.getXYPlot();
+        plot.addRangeMarker(new ValueMarker(SAFE_TEMP, Color.GREEN, new BasicStroke(2.0f)));
+        plot.setBackgroundPaint(Color.WHITE);
 
-        NumberAxis domainAxisXY = (NumberAxis) chartXY.getXYPlot().getDomainAxis();
-        domainAxisXY.setNumberFormatOverride(new NumberFormat() {
-            @Override
-            public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
-                if (toAppendTo == null) {
-                    toAppendTo = new StringBuffer();
-                }
 
-                int hours = (int) number / 3600;
-                int minutes = (int) (number % 3600) / 60;
-                int secs = (int) number % 60;
+        NumberAxis domainAxisXY = (NumberAxis) plot.getDomainAxis();
+//        domainAxisXY.setNumberFormatOverride();
+        domainAxisXY.setTickUnit(new NumberTickUnit(60 * 10,new TimeSeriesFormatter()));
 
-                toAppendTo
-                        .append(hours < 10 ? "0" : "")
-                        .append(hours)
-                        .append(":")
-                        .append(minutes < 10 ? "0" : "")
-                        .append(minutes)
-                        .append(":")
-                        .append(secs < 10 ? "0" : "")
-                        .append(secs);
-
-                return toAppendTo;
-            }
-
-            @Override
-            public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
-                return null;
-            }
-
-            @Override
-            public Number parse(String source, ParsePosition parsePosition) {
-                return null;
-            }
-        });
         ChartPanel chartPanelXY = new ChartPanel(chartXY);
         add(chartPanelXY);
+        validate();
 
+        findSafeTimes();
 
         validate();
+    }
+
+    private void findSafeTimes() {
+
+        for (int i = 0; i < datasetXY.getSeriesCount(); i++) {
+
+            XYSeries series = datasetXY.getSeries(i);
+            String description = series.getKey().toString();
+
+            double startTime = 0;
+
+            for (int j = 0; j < series.getItemCount(); j++) {
+                XYDataItem dataItem = series.getDataItem(j);
+
+                double xValue = dataItem.getXValue();
+                if (startTime == 0 && dataItem.getYValue() >= SAFE_TEMP) {
+                    startTime = xValue;
+
+                } else if (startTime != 0 && (dataItem.getYValue() < SAFE_TEMP
+                    || j == series.getItemCount() - 1)) {
+                    description += " " + (int) ((xValue - startTime) / 60) + "min and";
+
+
+                    Paint seriesPaint = plot.getRendererForDataset(datasetXY).getItemPaint(i,1);
+                    chartXY.getXYPlot().addDomainMarker(new IntervalMarker(startTime, xValue, seriesPaint, new BasicStroke(0.5f), seriesPaint, new BasicStroke(0.5f), 0.2f));
+
+                    startTime = 0;
+
+
+                }
+
+            }
+
+            if (description.endsWith("and")) {
+                description = description.substring(0, description.length() - 3);
+            }
+
+            log.info("{}:{}", series.getKey(), description);
+            series.setDescription(description);
+
+            //            add(new JTextField(description));
+        }
+    }
+
+    private void loadFiles() {
+        File dri = new File(".");
+        for (File file : dri.listFiles()) {
+            if (file.getName().endsWith(".csv")) {
+                try {
+                    datasetXY.addSeries(loadSeries(file));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private void matchMax() {
@@ -187,5 +215,34 @@ public class TimePlotter extends JFrame {
         }
 
         return series;
+    }
+
+    private static class TimeSeriesFormatter extends NumberFormat {
+        @Override
+        public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
+            if (toAppendTo == null) {
+                toAppendTo = new StringBuffer();
+            }
+
+            int hours = (int) number / 3600;
+            int minutes = (int) (number % 3600) / 60;
+            int secs = (int) number % 60;
+
+            toAppendTo.append(hours < 10 ? "0" : "").append(hours).append(":").append(
+                minutes < 10 ? "0" : "").append(minutes).append(":").append(
+                secs < 10 ? "0" : "").append(secs);
+
+            return toAppendTo;
+        }
+
+        @Override
+        public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
+            return null;
+        }
+
+        @Override
+        public Number parse(String source, ParsePosition parsePosition) {
+            return null;
+        }
     }
 }
